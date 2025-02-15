9af60ce0014deb25d4651c839abf0a2438c29bce
Richard Horton <richardhorton1312@gmail.com>
2025-02-15 20:23:53 +0000

    Added Player class

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,19 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private List<Card> hand = new List<Card>();
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}

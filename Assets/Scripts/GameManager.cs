b74df094402e0c4dd6dc8bcca8cb3bfeebc38bd4
Richard Horton <richardhorton1312@gmail.com>
2025-02-16 15:33:33 +0000

    Created initial player setup

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,14 +5,17 @@ public class GameManager : MonoBehaviour
 {
     private const int MAX_NUMBER_OF_PLAYERS = 8;
     private const int MIN_NUMBER_OF_PLAYERS = 2;
+    private const int HUMAN_PLAYER_INDEX = 0;
 
     private CardDeck deck;
     public Player[] players;
     private int numberOfPlayers = 2;
 
+    public Player[] players;
     void Start()
     {
         deck = new CardDeck();
+        SetNumberOfPlayers(4);
     }
 
     public void SetNumberOfPlayers(int numPlayers)
@@ -36,5 +39,15 @@ public class GameManager : MonoBehaviour
     void SetupPlayers()
     {
         players = new Player[numberOfPlayers];
+
+        for(int i = 0; i < numberOfPlayers; i++)
+        {
+            players[i] = gameObject.AddComponent<Player>();
+            players[i].game = this;
+        }
+
+        players[HUMAN_PLAYER_INDEX].SetAsPlayer(); // Set the first player in the list as the human player
+    }
+
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,9 +4,18 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     private List<Card> hand = new List<Card>();
+    private bool isOpponent = true;
+    
+    public GameManager game;
 
     void Awake()
     {
         
     }
+
+    public void SetAsPlayer()
+    {
+        isOpponent = false;
+    }
+
 }

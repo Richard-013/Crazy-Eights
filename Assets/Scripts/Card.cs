e26f23778c03469d0a2d0034984d0300fe854fd7
Richard Horton <richardhorton1312@gmail.com>
2025-02-16 15:29:33 +0000

    Added Convert Card to String
    Function takes cardNumber and cardSuit and creates a readable string from it

--- Assets/Scripts/Card.cs
+++ Assets/Scripts/Card.cs
@@ -18,4 +18,47 @@ public class Card
     {
         return cardSuit;
     }
+
+    public string ReadCard()
+    {
+        string cardStatement;
+
+        switch (cardNumber)
+        {
+            case 1:
+                cardStatement = "The Ace";
+                break;
+            case 11:
+                cardStatement = "The Jack";
+                break;
+            case 12:
+                cardStatement = "The Queen";
+                break;
+            case 13:
+                cardStatement = "The King";
+                break;
+            default:
+                cardStatement = "The " + cardNumber;
+                break;
+        }
+
+        switch (cardSuit)
+        {
+            case 0:
+                cardStatement = cardStatement + " of Clubs";
+                return  cardStatement;
+            case 1:
+                cardStatement = cardStatement + " of Diamonds";
+                return  cardStatement;
+            case 2:
+                cardStatement = cardStatement + " of Hearts";
+                return  cardStatement;
+            case 3:
+                cardStatement = cardStatement + " of Spades";
+                return  cardStatement;
+            default:
+                cardStatement = cardStatement + " of " + cardSuit;
+                return  cardStatement;
+        }
+    }
 }

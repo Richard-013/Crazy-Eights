edee5c8994ce88e662b38844d089b6a35bd10dd9
Richard Horton <richardhorton1312@gmail.com>
2025-02-20 01:00:52 +0000

    Outlined minor functionality elements in Opponent

--- Assets/Scripts/Opponent.cs
+++ Assets/Scripts/Opponent.cs
@@ -1,8 +1,28 @@
 public class Opponent : Player
 {
+    public override void AddCardToHand(Card newCard)
+    {
+        hand.Add(newCard);
+        newCard.faceHidden = true;
+        newCard.HideCardFace();
+    }
+
     public override Card Turn(Card recentCard)
     {
         lastPlayedCard = recentCard;
-        return hand[0];
+
+        Card playedCard = hand[0];
+        playedCard.faceHidden = false;
+        playedCard.ShowCardFace();
+
+        return playedCard;
+    }
+
+    protected override void DrawCard()
+    {
+        Card newCard = game.DrawCardForPlayer();
+        AddCardToHand(newCard);
+        newCard.HideCardFace();
+        //newCard.MoveCard();
     }
 }
